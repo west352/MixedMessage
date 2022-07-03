4051c37b0603ab34f9b045b9a0427929e9a8265e
Changjiang Liu <changjiangliu@Changjiangs-MacBook-Pro.local>
2022-07-03 12:49:15 -0700

    finish the program

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ const names = ["West", "Janel", "Bobo", "Pikachu"];
 const entertainment = ["tv show", "movie"];
 const tvShows = ["stranger things", "Pokemon", "Squid Game", "One Punch Man"];
 const movies = ["Avengers", "Avatar", "Justice League"];
+
+function randomSelector(array) {
+    let arrLength = array.length;
+    return array[Math.floor(Math.random()*arrLength)];
+}
+
+function messageGenerator() {
+    let name = randomSelector(names);
+    let type = randomSelector(entertainment);
+    let show;
+    if (type === "tv show") {
+        show = randomSelector(tvShows);
+    } else {
+        show = randomSelector(movies);
+    }
+    return (`${name}'s favorite ${type} is ${show}.`);
+}
+
+console.log(messageGenerator());
