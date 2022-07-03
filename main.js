5abf81c488315c8ecc45250cfbd0eb0ec36a5c4d
Changjiang Liu <changjiangliu@Changjiangs-MacBook-Pro.local>
2022-07-03 12:39:08 -0700

    create three arrays to store information

---
+++ main.js
@@ -0,0 +1,4 @@
+const names = ["West", "Janel", "Bobo", "Pikachu"];
+const entertainment = ["tv show", "movie"];
+const tvShows = ["stranger things", "Pokemon", "Squid Game", "One Punch Man"];
+const movies = ["Avengers", "Avatar", "Justice League"];
